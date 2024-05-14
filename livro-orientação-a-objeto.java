1ef77a2b6b92f9d983b8681509175f570117742c
Deyvis Ferreira <165066939+DeyvisFerreira@users.noreply.github.com>
2024-05-14 17:52:51 -0300

    Create livro-orientação-a-objeto.java

---
+++ livro-orientação-a-objeto.java
@@ -0,0 +1,33 @@
+// Classe Livro
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int numeroDePaginas;
+
+    public Livro(String titulo, String autor, int numeroDePaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroDePaginas = numeroDePaginas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getNumeroDePaginas() {
+        return numeroDePaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", numeroDePaginas=" + numeroDePaginas +
+                '}';
+    }
+}
